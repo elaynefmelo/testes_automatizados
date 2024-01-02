90c491a6506484641d7d4dc8efd436c3d7e86942
elaynefmelo <elayne.mello6@gmail.com>
2024-01-02 14:25:20 -0300

    Novos testes

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Elayne\Documents\TesteSelenium\venv
+set VIRTUAL_ENV=C:\Users\20211ewbj0302\Documents\testes_automatizados\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
